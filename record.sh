19f03c5700b33b965f16262e84bc021676165fbb
Diep Tran <69444682+Dieptranivsr@users.noreply.github.com>
2022-07-08 13:45:23 +0700

    Update record.sh

--- record.sh
+++ record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # camera,map,range,path,traj,cmd
-rosbag record /camera/pose /sdf_map/occupancy_inflate /sdf_map/update_range /planning_vis/trajectory /planning/position_cmd_vis /mavros/local_position/pose /camera/color/image_raw
+rosbag record /camera/pose /camera/color/image_raw /sdf_map/occupancy_inflate /sdf_map/update_range /planning_vis/trajectory /planning/position_cmd_vis /mavros/local_position/pose 
 
 
